ba20d89a9f727e9a4554ab05058cc8779fdacd2b
Erlkonig <vlpiatachenko@gmail.com>
2024-05-26 17:57:16 +0300

    fix  wrk wih new seup

--- evaluation.py
+++ evaluation.py
@@ -2,8 +2,6 @@ import os
 import torch
 import argparse
 from tqdm import tqdm
-
-
 from torchsummary import summary
 
 import utils.utils
@@ -22,15 +20,15 @@ if __name__ == '__main__':
 
     assert os.path.exists(opt.weights), "请指定正确的模型路径"
 
-    #打印消息
+    # 打印消息
     print("评估配置:")
-    print("model_name:%s"%cfg["model_name"])
-    print("width:%d height:%d"%(cfg["width"], cfg["height"]))
-    print("val:%s"%(cfg["val"]))
-    print("model_path:%s"%(opt.weights))
-    
-    #加载数据
-    val_dataset = utils.datasets.TensorDataset(cfg["val"], cfg["width"], cfg["height"], imgaug = False)
+    print("model_name:%s" % cfg["model_name"])
+    print("width:%d height:%d" % (cfg["width"], cfg["height"]))
+    print("val:%s" % (cfg["val"]))
+    print("model_path:%s" % (opt.weights))
+
+    # 加载数据
+    val_dataset = utils.datasets.TensorDataset(cfg["val"], cfg["width"], cfg["height"], imgaug=False)
 
     batch_size = int(cfg["batch_size"] / cfg["subdivisions"])
     nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])
@@ -44,22 +42,44 @@ if __name__ == '__main__':
                                                  drop_last=False,
                                                  persistent_workers=True
                                                  )
-    
-    #指定后端设备
+
+    # 指定后端设备
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    #初始化模型
+    # 初始化模型
     model = model.detector.Detector(cfg["classes"], cfg["anchor_num"], True).to(device)
-    model.load_state_dict(torch.load(opt.weights, map_location=device))
-    #sets the module in eval node
+
+    def adjust_state_dict_keys(state_dict, prefix_to_remove='module.'):
+        new_state_dict = {}
+        for k, v in state_dict.items():
+            if k.startswith(prefix_to_remove):
+                new_key = k[len(prefix_to_remove):]
+            else:
+                new_key = k
+            new_state_dict[new_key] = v
+        return new_state_dict
+
+    # Load the state dictionary
+    checkpoint = torch.load(opt.weights, map_location=device)
+
+    # Adjust the keys if necessary
+    if 'model_state_dict' in checkpoint:
+        state_dict = adjust_state_dict_keys(checkpoint['model_state_dict'])
+    else:
+        state_dict = adjust_state_dict_keys(checkpoint)
+
+    # Load the model state dictionary
+    model.load_state_dict(state_dict, strict=False)
+    
+    # 设置模型为评估模式
     model.eval()
 
-    #打印模型结构
+    # 打印模型结构
     summary(model, input_size=(3, cfg["height"], cfg["width"]))
-    
-    #模型评估
-    print("computer mAP...")
+
+    # 模型评估
+    print("computing mAP...")
     _, _, AP, _ = utils.utils.evaluation(val_dataloader, cfg, model, device)
-    print("computer PR...")
+    print("computing PR...")
     precision, recall, _, f1 = utils.utils.evaluation(val_dataloader, cfg, model, device, 0.3)
-    print("Precision:%f Recall:%f AP:%f F1:%f"%(precision, recall, AP, f1))
+    print("Precision:%f Recall:%f AP:%f F1:%f" % (precision, recall, AP, f1))
